3d1c59754624f820062ec7a2d5cc221998dd8da3
yeo-web <jyw325654@gmail.com>
2024-12-13 16:39:59 +0900

    🎉 Term Project backend 시작

--- src/main/kotlin/kr/ac/kumoh/s20231043/rhythm_game/controller/MessageController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20231043/rhythm_game/controller/MessageController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/Message")
+@RequestMapping("/api/message")
 class MessageController {
     @GetMapping
     @CrossOrigin(origins = ["http://localhost:3000"])
